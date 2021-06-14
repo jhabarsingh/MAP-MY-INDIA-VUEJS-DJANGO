972621d50e2f37233f98ca7c4d3e5d96be39fc2a
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-06-14 18:32:03 +0530

    Removed Proxy

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  devServer: {
-    proxy: {
-        'https://outpost.mapmyindia.com/': {
-          target: "http://localhost:8080/",
-          changeOrigin: true
-        }
-    }
-  },
   transpileDependencies: [
     'vuetify'
   ]
